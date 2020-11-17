1df00cb04edcea60a61891a83f7c236d688d8bc2
ZackAustin <zacksaustin@gmail.com>
2020-11-17 15:30:42 -0700

    Refreshed IG.

--- _refresh.bat
+++ _refresh.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SET tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
 SET input_cache_path=%~dp0input-cache
-SET resources_path=input/resources
+SET resources_path=%~dp0input/resources
 SET ig_resource_path=input/anc-cds.xml
 
 ECHO Checking internet connection...
