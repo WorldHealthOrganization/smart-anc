4539f440cd6f8b96c399de2da42852a60b8a7390
ZackAustin <zacksaustin@gmail.com>
2020-11-17 14:15:11 -0700

    Update refresh scripts.

--- _refresh.bat
+++ _refresh.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
-SET tooling_jar=tooling-1.0.2-SNAPSHOT-jar-with-dependencies.jar
+SET tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
 SET input_cache_path=%~dp0input-cache
-SET resources_path=%~dp0/input/resources
-SET ig_resource_path=%~dp0/input/opioid-cds.xml
+SET resources_path=input/resources
+SET ig_resource_path=input/anc-cds.xml
 
 ECHO Checking internet connection...
 PING tx.fhir.org -n 1 -w 1000 | FINDSTR TTL && GOTO isonline
@@ -12,18 +12,18 @@ GOTO igpublish
 
 :isonline
 ECHO We're online, setting publish to the Connectathon sandbox FHIR server
-SET fsoption=-fs http://cqm-sandbox.alphora.com/cqf-ruler-r4/fhir/
+SET fsoption=-fs http://localhost:8080/cqf-ruler-r4/fhir/
 
 :igpublish
 
 SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 IF EXIST "%input_cache_path%\%tooling_jar%" (
-	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -rp="%resources_path%" -igrp="%ig_resource_path%" -iv=fhir4 -t -d -p %fsoption%
-	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -ip=%~dp0 -rp="%resources_path%" -igrp="%ig_resource_path%" -iv=fhir4 -t -d -p %fsoption%
+	ECHO running: JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -rp="%resources_path%" -ip="%ig_resource_path%" -t -d -p %fsoption%
+	JAVA -jar "%input_cache_path%\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -rp="%resources_path%" -ip="%ig_resource_path%" -t -d -p %fsoption%
 ) ELSE If exist "..\%tooling_jar%" (
-	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -rp="%resources_path%" -igrp="%ig_resource_path%" -iv=fhir4 -t -d -p %fsoption%
-	JAVA -jar "..\%tooling_jar%" -RefreshIG -ip=%~dp0 -iv=fhir4 -rp="%resources_path%" -igrp="%ig_resource_path%" -t -d -p %fsoption%
+	ECHO running: JAVA -jar "..\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -rp="%resources_path%" -ip="%ig_resource_path%" -t -d -p %fsoption%
+	JAVA -jar "..\%tooling_jar%" -RefreshIG -root-dir=%~dp0 -rp="%resources_path%" -ip="%ig_resource_path%" -t -d -p %fsoption%
 ) ELSE (
 	ECHO IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 )
--- _refresh.sh
+++ _refresh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #DO NOT EDIT WITH WINDOWS
-tooling_jar=tooling-1.2.0-SNAPSHOT-jar-with-dependencies.jar
+tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
 input_cache_path=./input-cache
 resources_path=$PWD/input/resources
-ig_resource_path=./input/opioid-cds.xml
+ig_resource_path=./input/anc-cds.xml
 
 set -e
 echo Checking internet connection...
@@ -22,12 +22,12 @@ echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	JAVA -jar $tooling -RefreshIG -ip="$PWD" -igrp="$ig_resource_path" -rp="$resources_path" -iv=fhir4 -t -d -p $fsoption
+	JAVA -jar $tooling -RefreshIG -root-dir="$PWD" -ip="$ig_resource_path" -rp="$resources_path" -t -d -p $fsoption
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		JAVA -jar $tooling -RefreshIG -ip="$PWD" -igrp="$ig_resource_path" -rp="$resources_path" -iv=fhir4 -t -d -p $fsoption
+		JAVA -jar $tooling -RefreshIG -root-dir="$PWD" -ip="$ig_resource_path" -rp="$resources_path" -t -d -p $fsoption
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateCQFTooling.  Aborting...
 	fi
